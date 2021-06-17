f59a5368ab3427d84087b4f4bb0d3799e667f973
Hyeongjun Jo <chogudwns@naver.com>
2021-06-17 18:09:46 +0900

    recap+morgan

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
+// const loggermid = (req, res, next) => {
+//     console.log(`METHOD ${req.method} ${req.path}`);
+//     next();
+// };
 
 const handleHome = (req, res) => {
     return res.send("<h1>I still love you</h1>");
@@ -16,6 +18,7 @@ const handleHome = (req, res) => {
 
 //application 설정
 app.use(logger);
+// app.use(loggermid);
 app.get("/", handleHome);
 
 
