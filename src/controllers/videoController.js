d3af9264f27a526d972101004a9cf142e7e3f9e9
Hyeongjun Jo <chogudwns@naver.com>
2021-08-14 17:51:26 +0900

    edit_fixed(3)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -35,12 +35,16 @@ export const getEdit = async (req, res) => {
 };
 export const postEdit = async (req, res) => {
   const { id } = req.params;
+  const {
+    user: { _id },
+  } = req.session;
   const { title, description, hashtags } = req.body;
-  const video = await Video.exists({ _id: id }); //Boolean값 반환
+  const video = await Video.findById(id); //Boolean값 반환
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
-  if (String(video.owner) !== String(id)) {
+  if (String(video.owner) !== String(_id)) {
+    req.flash("error", "You are not the owner of the video.");
     return res.status(403).redirect("/");
   }
   await Video.findByIdAndUpdate(id, {
