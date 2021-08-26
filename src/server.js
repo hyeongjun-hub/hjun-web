7b061f21a32929c260a6ee79c72d9f4218a3dd91
Hyeongjun Jo <chogudwns@naver.com>
2021-08-26 19:22:09 +0900

    cross_origin-bug7

--- src/server.js
+++ src/server.js
@@ -13,6 +13,10 @@ const app = express();
 const logger = morgan("dev");
 
 app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://hyeongjuntube.s3.amazonaws.com"
+  );
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
   next();
