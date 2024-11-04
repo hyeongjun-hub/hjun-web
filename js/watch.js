476095fd81a0f4d8bc0968c6a4ccb1893dc7f2d5
hyeongjun-hub <hyeongjun-hub@users.noreply.github.com>
2024-11-04 01:05:15 +0000

    deploy: d57e0da85d1cd8b652da5f6e88e91aaca35cb14a

---
+++ js/watch.js
@@ -0,0 +1,1 @@
+(()=>{var e=document.querySelector(".mainContainer"),t=document.querySelector(".otherVideos"),o=e.offsetHeight;o!==t.offsetHeight&&(t.style.height=o+"px")})();
--- src/client/js/watch.js
+++
@@ -1,9 +0,0 @@
-const mainContent = document.querySelector(".mainContainer");
-const sidebar = document.querySelector(".otherVideos");
-
-const mainContentHeight = mainContent.offsetHeight;
-const sidebarHeight = sidebar.offsetHeight;
-
-if (mainContentHeight !== sidebarHeight) {
-  sidebar.style.height = mainContentHeight + "px";
-}
